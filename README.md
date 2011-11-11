a82033da0f88c475626e6cd6a760ba5d34c39d3e
Eero af Heurlin <eero.afheurlin@iki.fi>
2011-11-11 23:50:56 +0200

    stubs

---
+++ README.md
@@ -0,0 +1,4 @@
+# DS1307 library
+
+Simple lib for this RTC, uses the I2C host library instead of Wire.
+
---
+++ ds1307.h
@@ -0,0 +1,15 @@
+// safety againts double-include
+#ifndef ds1307_h
+#define ds1307_h
+#include <WProgram.h> 
+#include <i2c_device.h>
+
+// Stub extension for now
+class ds1307 : public i2c_device
+{
+};
+
+
+
+#endif
+// *********** END OF CODE **********
