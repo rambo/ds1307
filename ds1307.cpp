19e4d0b089a6b0be0f35fed30f4c9ef2899cd45e
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-01-24 10:54:25 +0200

    Revert "scripted arduino 1.0 changes (use standard C type names)"
    This reverts commit b72fa87abf557c61bd2a65b35b5bb9537ab4c0d1.

--- ds1307.cpp
+++ ds1307.cpp
@@ -1,19 +1,19 @@
 #include "ds1307.h"
 
 
-void ds1307::begin(uint8_t wire_begin)
+void ds1307::begin(boolean wire_begin)
 {
     i2c_device::begin(DS1307_ADDRESS, wire_begin);
 }
 
-uint8_t ds1307::read_clock()
+boolean ds1307::read_clock()
 {
     return read_many(0x0, 7, rtc_regs);
 }
 
-uint8_t ds1307::set_clock(uint8_t years, byte month, byte day, byte hour, byte minute, byte second)
+boolean ds1307::set_clock(byte years, byte month, byte day, byte hour, byte minute, byte second)
 {
-    uint8_t tmp[3];
+    byte tmp[3];
     // Seconds register (also clock-halt which we disable at this point)
     tmp[0] = ((second / 10) << 4) | (second % 10);
     // Minutes register
@@ -39,42 +39,42 @@ uint8_t ds1307::set_clock(uint8_t years, byte month, byte day, byte hour, byte m
     return true;
 }
 
-inline uint8_t parse_bcd(byte reg_value, byte high_mask)
+inline byte parse_bcd(byte reg_value, byte high_mask)
 {
     return ((reg_value & high_mask) >> 4) * 10 + (reg_value & B00001111);
 }
 
-uint8_t ds1307::second()
+byte ds1307::second()
 {
     //return ((rtc_regs[0] & B01110000) >> 4) * 10 + (rtc_regs[0] & B00001111);
     return parse_bcd(rtc_regs[0], B01110000);
 }
-uint8_t ds1307::minute()
+byte ds1307::minute()
 {
     //return ((rtc_regs[1] & B01110000) >> 4) * 10 + (rtc_regs[1] & B00001111);
     return parse_bcd(rtc_regs[1], B01110000);
 }
-uint8_t ds1307::hour()
+byte ds1307::hour()
 {
     //return ((rtc_regs[2] & B00110000) >> 4) * 10 + (rtc_regs[2] & B00001111);
     return parse_bcd(rtc_regs[2], B00110000);
 }
-uint8_t ds1307::dow()
+byte ds1307::dow()
 {
     //return (rtc_regs[3] & B00000111);
     return parse_bcd(rtc_regs[3], B00000000);
 }
-uint8_t ds1307::day()
+byte ds1307::day()
 {
     //return ((rtc_regs[4] & B00110000) >> 4) * 10 + (rtc_regs[4] & B00001111);
     return parse_bcd(rtc_regs[4], B00110000);
 }
-uint8_t ds1307::month()
+byte ds1307::month()
 {
     //return ((rtc_regs[5] & B00010000) >> 4) * 10 + (rtc_regs[5] & B00001111);
     return parse_bcd(rtc_regs[5], B00010000);
 }
-uint8_t ds1307::years()
+byte ds1307::years()
 {
     //return ((rtc_regs[6] & B11110000) >> 4) * 10 + (rtc_regs[6] & B00001111);
     return parse_bcd(rtc_regs[6], B11110000);
--- ds1307.h
+++ ds1307.h
@@ -16,23 +16,23 @@
 class ds1307 : public i2c_device
 {
     public:
-        void begin(uint8_t wire_begin);
-        uint8_t read_clock(); // This reads the RTC data to local buffer used by the 
-        uint8_t minute(); // 0-60
-        uint8_t second(); // 0-60
-        uint8_t hour(); // 0-23
-        uint8_t day(); // 1-31
-        uint8_t month(); // 1-12
-        uint8_t years(); // 0-99
+        void begin(boolean wire_begin);
+        boolean read_clock(); // This reads the RTC data to local buffer used by the 
+        byte minute(); // 0-60
+        byte second(); // 0-60
+        byte hour(); // 0-23
+        byte day(); // 1-31
+        byte month(); // 1-12
+        byte years(); // 0-99
         int year(); // DS1307_CENTURY + 0-99
-        uint8_t dow(); // 1-7
+        byte dow(); // 1-7
 #ifndef DS1307_CONSERVE_SPACE
         char* iso_ts(); // YYYY-MM-DD HH:MM:SS
 #endif
-        uint8_t set_clock(uint8_t years, byte month, byte day, byte hour, byte minute, byte second); // Note that years is 00-99
+        boolean set_clock(byte years, byte month, byte day, byte hour, byte minute, byte second); // Note that years is 00-99
     
     protected:
-        uint8_t rtc_regs[7];
+        byte rtc_regs[7];
 #ifndef DS1307_CONSERVE_SPACE
 		char isobuffer[20];
 #endif
